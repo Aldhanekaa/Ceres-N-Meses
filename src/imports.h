db39f10cce4d3f38a6085a4cddcdf3a3bb362c88
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-04-10 23:32:51 +0700

    a

---
+++ src/imports.h
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h> // minus ke ground, + ke 5V, dan tengah ke A0
+
+#ifndef _LED_DATA
+  #include <data/LED_DATA.h>
+#endif
+
+#ifndef _sensor_previous_data_union
+  #include <data/sensor_previous_data_union.h>
+#endif
+
+#ifndef __arrayOfData
+  #include <data/arrayOfDatas.h>
+#endif
+
+#include <utils/turn_led.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,8 @@
-#include <Arduino.h>
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h> // minus ke ground, + ke 5V, dan tengah ke A0
+#include <imports.h>
 
 #define DHTPIN A0 // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT11 // DHT 11
 #define LED 5
-
-#ifndef _LED_DATA
-  #include <data/LED_DATA.h>
-#endif
-
-#ifndef _sensor_previous_data_union
-  #include <data/sensor_previous_data_union.h>
-#endif
-
-#ifndef __arrayOfData
-  #include <data/arrayOfDatas.h>
-#endif
-
-#include <utils/turn_led.h>
-
-unsigned int Sunlight_intensity;
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
@@ -45,6 +26,8 @@ void setup() {
   Serial.println(F("DHTxx Unified Sensor Example"));
 
   pinMode(LED_GROWTH.LED_PIN, OUTPUT);
+    // pinMode(6, INPUT);
+
 }
 
 void loop() {
@@ -57,81 +40,66 @@ void loop() {
 
   // Get humidity event and print its value.
   dht.humidity().getEvent(&humidity);
-
-  Sunlight_intensity = analogRead(A1);
+  unsigned int Sunlight_intensity;
+  Sunlight_intensity = analogRead(A0);
   // Serial.println(Sunlight_intensity);
   // digitalWrite(true, &LED_GROWTH);
 
-
-  if (Sunlight_intensity != Sunlight_intensity_previous_data.dataInFloat && (Sunlight_intensity == 1022.00 && Sunlight_intensity_previous_data.dataInFloat == 1023.00))
-  {
-    Sunlight_intensity_previous_data.dataInFloat = Sunlight_intensity;
     Serial.print("Sunlight intensity: ");
-    Serial.println(Sunlight_intensity_previous_data.dataInFloat);
+    Serial.println(analogRead(Sunlight_intensity));
 
-    if (Sunlight_intensity >= 1000) {
-      turn_led(true, &LED_GROWTH);
-    }else if (temperature.temperature >= maksTemperature && Sunlight_intensity < 1000) {
-      turn_led(false, &LED_GROWTH);
-    }
-  }
 
-  if (isnan(temperature.temperature)) {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else {
+  // if (Sunlight_intensity != Sunlight_intensity_previous_data.dataInFloat)
+  // {
+  //   Sunlight_intensity_previous_data.dataInFloat = Sunlight_intensity;
+  //   Serial.print("Sunlight intensity: ");
+  //   Serial.println(Sunlight_intensity_previous_data.dataInFloat);
 
-    if (DHT11_PREVIOUS_Temperature_DATA.dataInFloat != temperature.temperature || (DHT11_PREVIOUS_Humidity_DATA.dataInFloat != humidity.relative_humidity && !isnan(humidity.relative_humidity))) {
+  //   if (Sunlight_intensity >= 1000) {
+  //     turn_led(true, &LED_GROWTH);
+  //   }else if (temperature.temperature >= maksTemperature && Sunlight_intensity < 1000) {
+  //     turn_led(false, &LED_GROWTH);
+  //   }
+  // }
 
-      Serial.println("=================================================");
-      Serial.print("event.temperature: ");
-      Serial.println(temperature.temperature);
-      Serial.print(F("Humidity: "));
-      Serial.print(humidity.relative_humidity);
-      Serial.println(F("%"));
+  // if (isnan(temperature.temperature)) {
+  //   Serial.println(F("Error reading temperature!"));
+  // }
+  // else {
 
-      Serial.println("=================================================\n\n");
+  //   if (DHT11_PREVIOUS_Temperature_DATA.dataInFloat != temperature.temperature || (DHT11_PREVIOUS_Humidity_DATA.dataInFloat != humidity.relative_humidity && !isnan(humidity.relative_humidity))) {
 
-      if (temperature.temperature < maksTemperature) {
-        turn_led(true, &LED_GROWTH);
-      }
-      else if (Sunlight_intensity < 1000)
-      {
-        turn_led(false, &LED_GROWTH);
-      }
-    }
+  //     Serial.println("=================================================");
+  //     Serial.print("event.temperature: ");
+  //     Serial.println(temperature.temperature);
+  //     Serial.print(F("Humidity: "));
+  //     Serial.print(humidity.relative_humidity);
+  //     Serial.println(F("%"));
 
-    if (DHT11_PREVIOUS_Temperature_DATA.dataInFloat != temperature.temperature) {
-      DHT11_PREVIOUS_Temperature_DATA.dataInFloat = temperature.temperature;
-    }
-  }
+  //     Serial.println("=================================================\n\n");
 
-  if (isnan(humidity.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else {
-    if (DHT11_PREVIOUS_Humidity_DATA.dataInFloat != humidity.relative_humidity) {
-      DHT11_PREVIOUS_Humidity_DATA.dataInFloat = humidity.relative_humidity;
-    }
-  }
+  //     if (temperature.temperature < maksTemperature) {
+  //       turn_led(true, &LED_GROWTH);
+  //     }
+  //     else if (Sunlight_intensity < 1000)
+  //     {
+  //       turn_led(false, &LED_GROWTH);
+  //     }
+  //   }
+
+  //   if (DHT11_PREVIOUS_Temperature_DATA.dataInFloat != temperature.temperature) {
+  //     DHT11_PREVIOUS_Temperature_DATA.dataInFloat = temperature.temperature;
+  //   }
+  // }
+
+  // if (isnan(humidity.relative_humidity)) {
+  //   Serial.println(F("Error reading humidity!"));
+  // }
+  // else {
+  //   if (DHT11_PREVIOUS_Humidity_DATA.dataInFloat != humidity.relative_humidity) {
+  //     DHT11_PREVIOUS_Humidity_DATA.dataInFloat = humidity.relative_humidity;
+  //   }
+  // }
 
   // delay(500);
 }
-
-// // if status is true, then LED will turn to on, but if status is off LED will turn to off
-// void turn_led(bool status, LED_DATA *previous_data) {
-//   // if the led is off and status is true then turn it on
-//   if (status)
-//   {
-//     digitalWrite(previous_data->LED_PIN, HIGH);
-//     previous_data->LED_PIN = true;
-//     Serial.println(previous_data->LED_PIN);
-
-//   }
-//   else if (!status)
-//   {
-//     digitalWrite(previous_data->LED_PIN, LOW);
-//     previous_data->LED_PIN = false;
-//     Serial.println(previous_data->LED_PIN);
-//   }
-// }
